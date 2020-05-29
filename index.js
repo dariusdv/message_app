25cf4e882557ee8e744451f7dbc142f6f110c45e
dariusdv <ddariusn@gmail.com>
2020-05-29 13:20:33 +0300

    Show message panel

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+
+// Destructuring window.location object. Unpacking hash property to new variable
+const { hash: encryptedHash } = window.location;
+
+const message = atob(encryptedHash.replace('#', ''));
+
+if (message) {
+  document.querySelector("#message-form").classList.add('hide');
+  document.querySelector("#message-show").classList.remove('hide');
+
+  document.querySelector('#message-header').innerHTML = message;
+}
+
+// Form action on submit
 document.querySelector('form').addEventListener('submit', event => {
   event.preventDefault();
 
+  // Switch forms
   document.querySelector("#message-form").classList.add('hide');
   document.querySelector("#link-form").classList.remove('hide');
 
   const messageInput = document.querySelector('#message-input');
+
+  // Base64 encoding
   const encrypted = btoa(messageInput.value);
 
   const linkInput = document.querySelector("#link-input");
   linkInput.value = `${window.location}#${encrypted}`;
+
+  // Auto select link input value
   linkInput.select();
 });
